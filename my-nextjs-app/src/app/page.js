0d50a6a8790d2d6e15225e8c22ee108992404503
Colin McGovern <colin.mcgovern@pennmedicine.upenn.edu>
2025-03-16 12:35:48 -0400

    added components that need to be filled in

--- my-nextjs-app/src/app/page.js
+++ my-nextjs-app/src/app/page.js
@@ -1,14 +1,35 @@
 "use client"; // This is a client component
 
-import Image from "next/image";
-import styles from "./page.module.css";
-
 import CardGame from "./components/cardGame";
+
+import React, { useState, useEffect } from 'react';
+
+const [phase, setPhase] = useState(0);
+
+const longTermGameSeed = 0
+const shortTermGameSeed = 1
+
+// TODO every middle phase should have 60s limit
+// Randomly have last three middle phases be in random order 
 
 export default function Home() {
   return (
     <div style={{ maxWidth: "1500px", margin: "0 auto" }}>
-      <CardGame />
+      {phase === 0 ? (
+        <Welcome />
+      ) : phase === 1 ? (
+        <CardGame showCardsOnly={true} seed={longTermGameSeed} />
+      ) : phase === 2 ? (
+        <DemographicsSurvey />
+      ) : phase === 3 ? (
+        <CardGame seed={shortTermGameSeed} />
+      ) : phase === 4 ? (
+        <NostolgiaSurvey />
+      ) : phase === 5 ? (
+        <CardGame doNotShowCards={true} seed={longTermGameSeed} />
+      ) : (
+        <ThankYou />
+      )}
     </div>
   );
 }
