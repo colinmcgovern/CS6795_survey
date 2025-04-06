7f10ee7f4aee6fb281486ff8fd7bf5f02e1056bc
Colin McGovern <colin.mcgovern@pennmedicine.upenn.edu>
2025-04-06 15:04:52 -0400

    change end text

--- src/app/components/ThankYou.jsx
+++ src/app/components/ThankYou.jsx
@@ -12,7 +12,7 @@ export default function ThankYou(nickname) {
 
     return (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
-            <h1>Welcome to my test (Part 6 of 6)</h1>
+            <h1>Thank You! (Part 6 of 6)</h1>
             <p>Thank you so much for participating in my survey.</p>
 
             <br/>
